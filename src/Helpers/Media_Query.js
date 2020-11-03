1f3cabd1f5a21b16f7c49ccc0bf6eeb08b12ed9b
unknown <kukuh.baiquni@gmail.com>
2020-11-03 23:16:53 +0700

    add media query

--- src/Helpers/Media_Query.js
+++ src/Helpers/Media_Query.js
@@ -1,5 +1,3 @@
-import { useMediaQuery } from 'react-responsive';
-
 export const Desktop = ({ children }) => {
     const isDesktop = useMediaQuery({ minWidth: 992 });
     return isDesktop ? children : null
