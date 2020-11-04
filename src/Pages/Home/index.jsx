6286c3904ed34d4a76674229fa83f2edc5ce43ab
unknown <kukuh.baiquni@gmail.com>
2020-11-04 21:53:40 +0700

    prepare slice category

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,12 +2,14 @@ import { Fragment } from 'react';
 import './home.scss';
 import Carousel from './Components/Carousel';
 import Categories from './Components/Categories';
+import Discount from './Components/Discount';
 
 const Home = () => {
     return (
         <Fragment>
             <Carousel />
             <Categories />
+            <Discount />
             <div style={{ height: 1000 }} />
         </Fragment>
     )
