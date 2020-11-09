8442f2755832f6856837b3864e037298815b182e
unknown <kukuh.baiquni@gmail.com>
2020-11-09 13:37:24 +0700

    add wave background & prepare slicing

---
+++ src/Pages/Home/Components/Popular/index.jsx
@@ -0,0 +1,16 @@
+import './popular.scss'
+import { ReactComponent as WaveBackground } from 'Assets/Illustrations/wave_background.svg'
+
+const Popular = () => {
+    return(
+        <div className='popular-fluid-background'>
+            {/* <WaveBackground /> */}
+            <div className="content-area">
+                xs
+            </div>
+            asd
+        </div>
+    )
+}
+
+export default Popular
